32b01fa64e5bffa0298ba1ead64c550d40e3abfc
haryu <hryau6@gmail.com>
2021-08-19 18:29:50 +0900

    fix: 使用していないマグレーションファイルを削除

--- db/migrate/20210819042935_add_role_to_members.rb
+++
@@ -1,5 +0,0 @@
-class AddRoleToMembers < ActiveRecord::Migration[6.1]
-  def change
-    add_column :members, :role, :integer
-  end
-end
--- db/migrate/20210819083132_remove_role_from_members.rb
+++
@@ -1,5 +0,0 @@
-class RemoveRoleFromMembers < ActiveRecord::Migration[6.1]
-  def change
-    remove_column :members, :role, :integer, default: 0
-  end
-end
