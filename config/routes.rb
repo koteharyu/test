8ff10c143cacb0c783351d75214c19ffe8211294
haryu <hryau6@gmail.com>
2021-08-20 10:53:54 +0900

    modify: root to parties#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'parties#index'
   resources :parties do
     member do
       put :add_member
