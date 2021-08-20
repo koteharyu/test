200c8960d0dc12231c7f81520f89c906641d4efb
haryu <hryau6@gmail.com>
2021-08-20 11:38:08 +0900

    remove: json関係とデフォルトでのコメントを削除

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -1,14 +1,10 @@
 class PartiesController < ApplicationController
   before_action :set_party, only: [:show, :edit, :update, :destroy]
 
-  # GET /groups
-  # GET /groups.json
   def index
     @parties = Party.all
   end
 
-  # GET /groups/1
-  # GET /groups/1.json
   def show
     members = Member.all
     @not_participated_members = []
@@ -20,33 +16,21 @@ class PartiesController < ApplicationController
     end
   end
 
-  # GET /groups/new
   def new
     @party = Party.new
   end
 
-  # GET /groups/1/edit
   def edit
   end
 
-  # POST /groups
-  # POST /groups.json
   def create
     @party = Party.new(party_params)
-
-    respond_to do |format|
-      if @party.save
-        format.html { redirect_to @party, notice: 'party was successfully created.' }
-        format.json { render :show, status: :created, location: @party }
-      else
-        format.html { render :new }
-        format.json { render json: @party.errors, status: :unprocessable_entity }
-      end
+    if @party.save
+      redirect_to @party, notice: 'party was successfully created.'
+    else
+      render :new
     end
   end
-
-  # PATCH/PUT /partys/1
-  # PATCH/PUT /partys/1.json
 
   def update
     if @party.update(party_params)
@@ -55,20 +39,18 @@ class PartiesController < ApplicationController
       render :edit
     end
   end
-  # DELETE /partys/1
-  # DELETE /partys/1.json
+
   def destroy
     @party.destroy
     redirect_to parties_path, notice: 'deleted'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_party
       @party = Party.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def party_params
       params.require(:party).permit(:name)
     end
--- app/views/parties/_party.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! party, :id, :name, :created_at, :updated_at
-json.url party_url(party, format: :json)
