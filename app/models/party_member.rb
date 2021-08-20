b05d80f3e2c05ba16a7b87052c6e4120db128e54
haryu <hryau6@gmail.com>
2021-08-20 11:59:40 +0900

    add: validation グループに対して幹事は１人だけになるように

--- app/models/party_member.rb
+++ app/models/party_member.rb
@@ -2,5 +2,6 @@ class PartyMember < ApplicationRecord
   belongs_to :party
   belongs_to :member
 
+  validates :role, uniqueness: { scope: [:party_id, :member_id] }
   enum role: { regular: 0, organizer:1 }
 end
