540f56f9ede859391b9b0e39a30843bf1e0e035f
haryu <hryau6@gmail.com>
2021-08-20 11:29:36 +0900

    add: vparty & member の nameカラムに対してpresence: trueを付与

--- app/models/member.rb
+++ app/models/member.rb
@@ -2,4 +2,6 @@ class Member < ApplicationRecord
   has_many :party_members, dependent: :destroy
   has_many :parties, through: :party_members
   has_many :role, through: :role
+
+  validates :name, presence: true
 end
